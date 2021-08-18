78feaa2cc93f370a6572ff362f2fa29eb6bb2844
seonglae <sungle3737@gmail.com>
2021-08-19 02:51:13 +0900

    refactor: fix if single sentance

--- src/pages/api/search-notion.ts
+++ src/pages/api/search-notion.ts
@@ -4,14 +4,11 @@ import * as types from '~/lib/types'
 import { search } from '~/lib/notion'
 
 const searchNotion = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log(req)
+  console.log('Search', req)
   if (req.method !== 'POST') return res.status(405).send({ error: 'method not allowed' })
-
   const searchParams: types.SearchParams = req.body
-
   console.log('lambda search-notion', searchParams)
   const results = await search(searchParams)
-
   res.setHeader('Cache-Control', 'public, s-maxage=60, max-age=60, stale-while-revalidate=60')
   res.status(200).json(results)
 }
