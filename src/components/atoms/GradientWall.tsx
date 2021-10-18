c99f52748753baaa1df243254f2cb82527590ac9
seonglae <sungle3737@gmail.com>
2021-10-18 23:52:00 +0900

    feat: scroll controls components ecept remove

--- src/components/atoms/GradientWall.tsx
+++ src/components/atoms/GradientWall.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { a } from '@react-spring/three'
 import { useThree } from '@react-three/fiber'
+import { useScroll } from '@react-three/drei'
 
-export default function GradientWall({ color }) {
+export default function GradientWall({ color }: { color: any }) {
   const viewport = useThree(state => state.viewport)
+  const data = useScroll()
   return (
     <mesh scale={[viewport.width, viewport.height, 1]}>
-      <planeGeometry attach="geometry" args={[1, 1]} />
+      <planeGeometry attach="geometry" args={[1, data.pages * 2]} />
       <a.meshBasicMaterial attach="material" color={color} depthTest={false} />
     </mesh>
   )
