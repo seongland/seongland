9f6ab7c4ae0f5dce8876deb7adbfbb6cc40cd8fd
seonglae <sungle3737@gmail.com>
2022-03-16 00:59:08 +0900

    fix: remove loading component & sync deps

--- src/components/templates/ErrorPage.tsx
+++ src/components/templates/ErrorPage.tsx
@@ -1,28 +1,12 @@
 import Head from 'next/head'
 import * as React from 'react'
-import Footer from '@/components/molecules/Footer'
-import useDarkMode from 'use-dark-mode'
 
-export const ErrorPage: React.FC<{ statusCode: number; title?: string; subtitle?: string }> = ({
+export const ErrorPage: React.FC<{ statusCode?: number; title?: string; subtitle?: string }> = ({
   statusCode,
   title,
   subtitle,
 }) => {
   if (!title) title = String(statusCode)
-  const darkMode = useDarkMode(false, {
-    classNameDark: 'dark',
-    classNameLight: 'light',
-    onChange: isDark => {
-      if (isDark) {
-        document.documentElement.classList.remove('light')
-        document.documentElement.classList.add('dark')
-      } else {
-        document.documentElement.classList.remove('dark')
-        document.documentElement.classList.add('light')
-      }
-    },
-  })
-
   return (
     <>
       <Head>
@@ -33,7 +17,7 @@ export const ErrorPage: React.FC<{ statusCode: number; title?: string; subtitle?
       </Head>
 
       <div text="dark:white" className="flex absolute inset-0" justify="center">
-        <main className="flex items-center" justify="center" flex="col">
+        <main className="flex items-center container mx-10" text="center" justify="center" flex="col">
           <h1 text="5xl" font="semibold">
             {title}
           </h1>
@@ -43,10 +27,6 @@ export const ErrorPage: React.FC<{ statusCode: number; title?: string; subtitle?
             </h2>
           )}
         </main>
-      </div>
-
-      <div className="absolute bottom-0" w="full">
-        <Footer isDarkMode={darkMode.value} toggleDarkMode={darkMode.toggle} />
       </div>
     </>
   )
