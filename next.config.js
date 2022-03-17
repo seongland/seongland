3ebb86123714819c709ca0eaea50e08641b9213f
Restyled.io <commits@restyled.io>
2022-03-17 19:37:54 +0000

    refactor: restyled by prettier

--- next.config.js
+++ next.config.js
@@ -1,33 +1,32 @@
 //@ts-check
-const withTM = require('next-transpile-modules')([ 'three' ])
-const WindiCSS = require('windicss-webpack-plugin')
-                     .default
+const withTM = require('next-transpile-modules')(['three'])
+const WindiCSS = require('windicss-webpack-plugin').default
 
-                 /**
-                  * @type {import('next').NextConfig}
-                  */
-                 const nextConfig = {
-  headers : async () => [{
-    source : '/(.*)',
-    headers : [
-      {key : 'X-Frame-Options', value : 'DENY'},
-      {key : 'X-XSS-Protection', value : '1'},
-      {key : 'X-Content-Type-Options', value : 'nosniff'},
-    ],
-  },
-],
-  webpack : config => {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [
+        { key: 'X-Frame-Options', value: 'DENY' },
+        { key: 'X-XSS-Protection', value: '1' },
+        { key: 'X-Content-Type-Options', value: 'nosniff' },
+      ],
+    },
+  ],
+  webpack: config => {
     config.plugins.push(new WindiCSS())
     config.externals.push('sharp')
     return config
   },
-  eslint : {
-    ignoreDuringBuilds : true,
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-  pageDataCollectionTimeout : 2000,
-  reactStrictMode : true,
-  rewrites :
-      async () => [{source : '/social.png', destination : '/api/social-image'}],
+  pageDataCollectionTimeout: 2000,
+  reactStrictMode: true,
+  rewrites: async () => [{ source: '/social.png', destination: '/api/social-image' }],
 }
 
-                                    module.exports = withTM(nextConfig)
+module.exports = withTM(nextConfig)
--- site-config.js
+++ site-config.js
@@ -1,35 +1,35 @@
 const pkg = require('./package.json')
 
 const descriptionMd = pkg.description
-const description =
-    descriptionMd.replace(/\[([^\]]+)\](\([^)]+\)|\[[^\]]+\])/g, '$1')
-        .replace(/\n/g, '')
-        .replace(/\s{2,}/g, ' ')
-        .trim()
+const description = descriptionMd
+  .replace(/\[([^\]]+)\](\([^)]+\)|\[[^\]]+\])/g, '$1')
+  .replace(/\n/g, '')
+  .replace(/\s{2,}/g, ' ')
+  .trim()
 
 module.exports = {
-  title : pkg.displayName,
-  name : pkg.displayName,
+  title: pkg.displayName,
+  name: pkg.displayName,
   descriptionMd,
   description,
 
   // Personal Info
-  url : `https://${pkg.domain}`,
-  domain : pkg.domain,
-  previous : pkg.author.url,
-  author : pkg.author.name,
-  email : pkg.author.email,
+  url: `https://${pkg.domain}`,
+  domain: pkg.domain,
+  previous: pkg.author.url,
+  author: pkg.author.name,
+  email: pkg.author.email,
 
   // Social
-  twitterUsername : pkg.author.twitter.name,
-  twitter : pkg.author.twitter.name,
-  github : pkg.author.github.name,
-  linkedin : pkg.author.linkedin.name,
-  socials : {
-    GitHub : pkg.repository,
-    Twitter : pkg.author.twitter.name,
+  twitterUsername: pkg.author.twitter.name,
+  twitter: pkg.author.twitter.name,
+  github: pkg.author.github.name,
+  linkedin: pkg.author.linkedin.name,
+  socials: {
+    GitHub: pkg.repository,
+    Twitter: pkg.author.twitter.name,
   },
 
   // Color
-  bgColor : pkg.color,
+  bgColor: pkg.color,
 }
