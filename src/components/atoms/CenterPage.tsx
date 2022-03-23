6f08b647c0b04e60b75271e8c7f6048a2b38e36a
seonglae <sungle3737@gmail.com>
2022-03-24 02:24:17 +0900

    fix: height of scroll page responsive mapping issue resolved

--- src/components/atoms/CenterPage.tsx
+++ src/components/atoms/CenterPage.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { useScroll } from '@react-three/drei'
 
-export const CenterPage: React.FC<{ top: string; children: React.ReactNode }> = ({ top, children }) => {
+export const CenterPage: React.FC<{ children: React.ReactNode; page: number; pages: number }> = ({
+  page,
+  pages,
+  children,
+}) => {
+  // Pages
+  const state = useScroll()
+  const pageStart: (page: number) => string = page => {
+    const first = 0
+    const last = state.fill.clientHeight - state.fixed.clientHeight
+    return `${((last - first) / (pages - 1)) * (page - 1)}px`
+  }
+  const centered = useRef<HTMLDivElement>(null)
+  useFrame(() => {
+    if (centered.current) centered.current.style.top = pageStart(page)
+  })
+
   return (
-    <div className="flex absolute inset-0" justify="center" style={{ top }} h="100vh" w="100vw">
+    <div
+      ref={centered}
+      className="flex absolute inset-0"
+      justify="center"
+      style={{ top: pageStart(page) }}
+      h="100vh"
+      w="100vw">
       <div className="flex items-center container" justify="center">
         {children}
       </div>
