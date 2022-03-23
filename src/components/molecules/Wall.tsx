bda94047708154610bc004093d9a940a40bd6a1b
seonglae <sungle3737@gmail.com>
2022-03-24 02:23:43 +0900

    style: variable declaration & diable user select

--- src/components/molecules/Wall.tsx
+++ src/components/molecules/Wall.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { a } from '@react-spring/three'
 import { useThree } from '@react-three/fiber'
 import { useScroll } from '@react-three/drei'
-import { Interpolation } from '@react-spring/three'
+import type { Interpolation } from '@react-spring/three'
 
 export default function GradientWall({ color }: { color: Interpolation<number, string> }) {
   const viewport = useThree(state => state.viewport)
