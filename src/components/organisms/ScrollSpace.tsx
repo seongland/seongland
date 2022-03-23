b60bb0969ec33556f2742467889cd57c90c8a7a2
seonglae <sungle3737@gmail.com>
2022-03-24 02:23:43 +0900

    style: variable declaration & diable user select

--- src/components/organisms/ScrollSpace.tsx
+++ src/components/organisms/ScrollSpace.tsx
@@ -3,21 +3,21 @@ import { useThree, useFrame } from '@react-three/fiber'
 import { useScroll } from '@react-three/drei'
 import { useSpring } from '@react-spring/three'
 
-import Stars from '@/components/molecules/Stars'
+import { Stars } from '@/components/molecules/Stars'
 import GradientWall from '@/components/molecules/Wall'
 
 const DARK_WALL = ['#23262a', '#424242', '#232424', '#23262a']
 const LIGHT_WALL = ['#fff', '#fff', '#fff', '#fff']
+const INTERVAL = 30
 
 export const ScrollSpace: React.FC<{ isDarkMode: boolean }> = ({ isDarkMode }: { isDarkMode: boolean }) => {
-  const renderPer = 30
-  const height = useThree(state => Math.floor(state.size.height / renderPer) * renderPer)
+  const height = useThree(state => Math.floor(state.size.height / INTERVAL) * INTERVAL)
   const data = useScroll()
   const scrollMax = height * data.pages
 
   // Dark Mode
   const colorSet = useMemo(() => (isDarkMode ? DARK_WALL : LIGHT_WALL), [isDarkMode])
-  const [{ top }, set] = useSpring(() => ({ top: 0, mouse: [0, 0] }))
+  const [{ top }, set] = useSpring(() => ({ top: 0 }))
   useFrame(() => set.start({ top: data.range(0, 1) * scrollMax }))
 
   // Spring Props
