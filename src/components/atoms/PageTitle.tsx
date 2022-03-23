26a184a05f20653c0ccf73df6ddac0b04ad8fe67
seonglae <sungle3737@gmail.com>
2022-03-24 02:23:43 +0900

    style: variable declaration & diable user select

--- src/components/atoms/PageTitle.tsx
+++ src/components/atoms/PageTitle.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Image from 'next/image'
 
 export const PageTitle: React.FC<{ title: string }> = ({ title }) => {
   return (
-    <h1 text="5vh center" font="900" w="full" className="absolute top-15vh">
-      {title}
-    </h1>
+    <div className="flex absolute top-15vh" font="900">
+      <div w="5vh" h="5vh" m="0.5">
+        <Image layout="responsive" width="1" height="1" src="/seongland.svg" />
+      </div>
+      <span text="5vh center" w="full">
+        {title}
+      </span>
+    </div>
   )
 }
