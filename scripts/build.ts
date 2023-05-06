993bc36adffc2649475cb65c1d4e6782ffdb9b9d
seonglae <sungle3737@gmail.com>
2023-05-06 22:18:44 +0900

    deps: update packages & remove dynamic favicon

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,11 +1,7 @@
 import copyPublicAssets from './copy-public-assets'
-import generateFavicons from './generate-favicons'
 
 async function build() {
-  const promises = []
-  promises.push(await generateFavicons())
-  promises.push(await copyPublicAssets())
-  await Promise.all(promises)
+  await copyPublicAssets()
 }
 
 build()
