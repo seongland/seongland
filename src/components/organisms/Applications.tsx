aea1033819e90f6a729b554cb385657865762497
seonglae <sungle3737@gmail.com>
2023-05-07 02:10:12 +0900

    perf: dynamic loading heavy components

--- src/components/organisms/Applications.tsx
+++ src/components/organisms/Applications.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import CardGrid from '@/components/molecules/CardGrid'
+import dynamic from 'next/dynamic'
+
+export const CardGrid = dynamic(async () => import('@/components/molecules/CardGrid'))
 
 const cards = [
   {
     title: 'Intuiter',
     subtitle: 'productivity app for Windows',
     url: 'https://intuiter.vercel.app',
-    icon: 'icon/intuiter-outline.png',
-    background: '#ddd',
+    background: '/icon/intuiter.png',
+    theme: '#ddd',
     color: 'black',
     ratio: '50%',
   },
@@ -15,8 +17,8 @@ const cards = [
     title: 'To Spotify',
     subtitle: 'web 3d explorer',
     url: 'https://2spotify.vercel.app',
-    icon: 'icon/2spotify.svg',
-    background: '#111111',
+    background: '/icon/2spotify.svg',
+    theme: '#111111',
     color: 'white',
     ratio: '50%',
   },
@@ -24,10 +26,10 @@ const cards = [
     title: 'Screencast',
     subtitle: 'status of my web services',
     url: 'https://github.com/seonglae/screencast',
-    icon: 'image/screencast.gif',
-    background: '#2f3437',
+    background: '/image/screencast.gif',
+    theme: '#2f3437',
     color: 'white',
-    ratio: '300%',
+    ratio: '100%',
   },
 ]
 
