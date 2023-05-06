12aff7ee166b937fd5a70a9cb2652b0c9ac39e27
seonglae <sungle3737@gmail.com>
2023-05-07 03:44:45 +0900

    feat: card blur image with lazy load by SSG

--- src/components/atoms/PageTitle.tsx
+++ src/components/atoms/PageTitle.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
 
+import seongland from '~/public/seongland.png'
+
 export const PageTitle: React.FC<{ title: string }> = ({ title }) => {
   return (
     <div className="flex absolute top-15vh" font="900">
@@ -8,7 +10,10 @@ export const PageTitle: React.FC<{ title: string }> = ({ title }) => {
         <Image
           fill
           alt={title}
-          src="/seongland.svg"
+          src={seongland.src}
+          placeholder="blur"
+          loading="lazy"
+          blurDataURL={seongland.blurDataURL}
           sizes="(max-width: 768px) 50vw,
               (max-width: 1200px) 25vw,
               20vw"
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,7 +24,7 @@ function App(props: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/seongland.svg" />
+        <link rel="icon" href="/seongland.png" />
         <meta property="fb:app_id" content="419108182355029" />
       </Head>
 
