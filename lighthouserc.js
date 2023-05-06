6f0b5794d9b53fd92da0f45370c35729e00de807
seonglae <sungle3737@gmail.com>
2023-05-07 02:07:20 +0900

    meta: lighthouserc added for performance testing

---
+++ lighthouserc.js
@@ -0,0 +1,55 @@
+const { config } = require('dotenv')
+
+config()
+const root = 'http://localhost:8080'
+
+const configuration = {
+  ci: {
+    collect: {
+      url: [root],
+      numberOfRuns: 1,
+      startServerCommand: 'pnpm start',
+      upload: process.env.LHCI_TOKEN
+        ? {
+            target: 'lhci',
+            serverBaseUrl: 'https://lhci.fly.dev',
+          }
+        : {
+            target: 'temporary-public-storage',
+          },
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'bf-cache': 'warn',
+        'csp-xss': 'warn',
+        'link-name': 'warn',
+        'offscreen-images': 'warn',
+        'document-title': 'warn',
+        'image-alt': 'warn',
+        'errors-in-console': 'warn',
+        'maskable-icon': 'warn',
+        'color-contrast': 'warn',
+        'unsized-images': 'warn',
+        'inspector-issues': 'warn',
+        'uses-rel-preconnect': 'warn',
+        'tap-targets': 'warn',
+        'image-aspect-ratio': 'warn',
+        'lcp-lazy-loaded': 'warn',
+        'service-worker': 'warn',
+        'unused-css-rules': 'warn',
+        'unused-javascript': 'warn',
+        'heading-order': 'warn',
+        'themed-omnibox': 'warn',
+        'meta-description': 'warn',
+        'total-byte-weight': 'warn',
+        'uses-responsive-images': 'warn',
+        'robots-txt': 'warn',
+        'efficient-animated-content': 'warn',
+        canonical: 'warn',
+      },
+    },
+  },
+}
+
+module.exports = configuration
