e5c4050f388b333b7075f2052e6b30f1d0fcd785
seonglae <sungle3737@gmail.com>
2023-05-07 02:10:12 +0900

    perf: dynamic loading heavy components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import type { NextPage } from 'next'
 
-import { ScrollPage } from '@/components/templates/ScrollPage'
+import dynamic from 'next/dynamic'
+
 import { TypeTitle } from '@/components/atoms/TypeTitle'
 import { PageTitle } from '@/components/atoms/PageTitle'
-import { CenterPage } from '@/components/atoms/CenterPage'
 import { Applications } from '@/components/organisms/Applications'
-import { Others } from '@/components/organisms/Others'
-import { WebApps } from '@/components/organisms/WebApps'
+import { Publication } from '@/components/organisms/Publication'
+import { WebPages } from '@/components/organisms/WebPages'
+
+export const CenterPage = dynamic(async () => import('@/components/atoms/CenterPage'))
+export const ScrollPage = dynamic(async () => import('@/components/templates/ScrollPage'))
 
 const HEIGHT = 7
 const PAGES = 4
@@ -20,11 +23,11 @@ const Index: NextPage = () => {
       </CenterPage>
       <CenterPage page={2} pages={PAGES}>
         <PageTitle title="Web Pages" />
-        <WebApps />
+        <WebPages />
       </CenterPage>
       <CenterPage page={3} pages={PAGES}>
-        <PageTitle title="Small Projects" />
-        <Others />
+        <PageTitle title="Publication" />
+        <Publication />
       </CenterPage>
       <CenterPage page={4} pages={PAGES}>
         <PageTitle title="Applications" />
