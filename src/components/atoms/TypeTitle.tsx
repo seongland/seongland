3013191b129969dadcf813182313048abdc36344
seonglae <sungle3737@gmail.com>
2023-05-12 02:30:40 +0900

    style: type title gradient animation text

--- src/components/atoms/TypeTitle.tsx
+++ src/components/atoms/TypeTitle.tsx
@@ -6,11 +6,11 @@ export const Typed = dynamic(async () => import('react-typed'))
 export const TypeTitle: React.FC = () => {
   return (
     <Typed
-      className="text-5rem <lg:text-2.5rem font-900 text-center absolute top-42.5vh select-none"
+      className="text-4rem font-900 <lg:text-2.5rem text-center absolute top-41vh select-none"
       fadeOut={true}
       startDelay={100}
       typeSpeed={50}
-      strings={['Welcome to Seongland', 'Projects are below', 'Activities are below', 'Seongland']}
+      strings={['Welcome', 'Projects are below', 'Activities are below', 'Seongland']}
     />
   )
 }
