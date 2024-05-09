3e59e69ef08e336ad87c89d2c03a332a9f43c190
seonglae <sungle3737@gmail.com>
2024-05-10 01:58:46 +0900

    config: fix cards metadata into AI focused

---
+++ src/components/molecules/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import dynamic from 'next/dynamic'
+import Link from 'next/link'
+
+export const ThemeBtn = dynamic(async () => import('../atoms/ThemeBtn'))
+export const BGMBtn = dynamic(async () => import('../atoms/BGMBtn'))
+
+export const Header: React.FC = () => {
+  return (
+    <header
+      className="flex items-center"
+      text="dark:white"
+      m="x-auto t-auto b-0"
+      p="4"
+      w="full max-10/12 <sm:max-12/12"
+      justify="between"
+      flex="row <sm:col"
+      select="none">
+      <div select="none" text="md" order="3" className="items-center flex">
+        <Link id="author" font="900 normal" href="https://texonom.com/portfolio" text="underline">
+          Portfolio
+        </Link>
+      </div>
+      <div order="1"></div>
+      <div order="2"></div>
+    </header>
+  )
+}
