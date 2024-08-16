bbf9a1ddd6d8252f66716197f83fd43b5e2c058e
seonglae <sungle3737@gmail.com>
2024-08-17 00:02:50 +0900

    fix: eslint issue with extending react types

--- src/types/react.d.ts
+++ src/types/react.d.ts
@@ -1,5 +1,6 @@
 import { AttributifyAttributes } from 'windicss/types/jsx'
 
 declare module 'react' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface HTMLAttributes extends AttributifyAttributes {}
 }
