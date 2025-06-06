2313878b11f6a9ff84ebf32c485a62c4d8b21249
Seonglae Cho <sungle3737@gmail.com>
2025-06-06 15:46:54 +0100

    feat: add blur transition on landing background

--- src/components/templates/ScrollPage.tsx
+++ src/components/templates/ScrollPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
 import { Canvas } from '@react-three/fiber'
 import { ScrollControls, Scroll, useScroll } from '@react-three/drei'
@@ -16,21 +16,29 @@ export const ScrollPage: React.FC<{ height: number; damping?: number; children: 
   const { theme } = useThemes()
   useScroll()
 
+  const [blur, setBlur] = useState(true)
+  useEffect(() => {
+    const timer = setTimeout(() => setBlur(false), 700)
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <>
       <Head>
         <meta name="theme-color" content={bgColor[theme]} />
       </Head>
-      <Canvas>
-        <ScrollControls damping={damping} pages={height}>
-          <Scroll>
-            <ScrollSpace />
-          </Scroll>
-          <Scroll html>
-            <div text="dark:white"> {children}</div>
-          </Scroll>
-        </ScrollControls>
-      </Canvas>
+      <div className={`transition duration-700 ${blur ? 'blur-sm' : ''}`}>
+        <Canvas>
+          <ScrollControls damping={damping} pages={height}>
+            <Scroll>
+              <ScrollSpace />
+            </Scroll>
+            <Scroll html>
+              <div text="dark:white"> {children}</div>
+            </Scroll>
+          </ScrollControls>
+        </Canvas>
+      </div>
     </>
   )
 }
