784938b76160048c934b89b8dcf7737d80c1efa8
Seonglae Cho <sungle3737@gmail.com>
2025-06-15 20:59:13 +0100

    Replace heavy star shader with performant Sparkles

--- src/components/molecules/GalaxyShader.tsx
+++ src/components/molecules/GalaxyShader.tsx
@@ -1,47 +1,10 @@
-import React, { useRef } from 'react'
-import { useFrame, extend } from '@react-three/fiber'
-import { shaderMaterial } from '@react-three/drei'
-import * as THREE from 'three'
+import React from 'react'
+import { Sparkles } from '@react-three/drei'
 
-const StarfieldMaterial = shaderMaterial(
-  { uTime: 0 },
-  `
-  varying vec2 vUv;
-  void main() {
-    vUv = uv;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-  }
-  `,
-  `
-  uniform float uTime;
-  varying vec2 vUv;
-  
-  float random(vec2 st) {
-    return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
-  }
-  
-  void main() {
-    float n = random(vUv * 100.0 + uTime * 0.1);
-    float star = step(0.996, n);
-    gl_FragColor = vec4(vec3(star), star);
-  }
-  `,
+export const GalaxyShader: React.FC<JSX.IntrinsicElements['group']> = props => (
+  <group {...props}>
+    <Sparkles count={1500} scale={[100, 100, 100]} size={2} speed={0.4} color="#b0c6ff" />
+  </group>
 )
-
-extend({ StarfieldMaterial })
-
-export const GalaxyShader: React.FC<JSX.IntrinsicElements['mesh']> = props => {
-  const material = useRef<any>()
-  useFrame((_, delta) => {
-    if (material.current) material.current.uTime += delta
-  })
-  return (
-    <mesh {...props}>
-      <sphereGeometry args={[50, 32, 32]} />
-      {/* @ts-ignore */}
-      <starfieldMaterial ref={material} key={StarfieldMaterial.key} side={THREE.BackSide} />
-    </mesh>
-  )
-}
 
 export default GalaxyShader
