a3d0aeae7d24a064dd9b352e5e7737c39e3e605f
seonglae <sungle3737@gmail.com>
2026-02-03 16:34:12 +0000

    chore: cleanup trigger comment

--- next.config.js
+++ next.config.js
@@ -74,4 +74,3 @@ const nextConfig = {
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: process.env.ANALYZE === 'true' })
 module.exports = withBundleAnalyzer(nextConfig)
-// vercel trigger 1770136060
